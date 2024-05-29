c3376e762f17b3d28a50834461b76fadd1e3b24d
Unlok-Simon <tyrrells@gmail.com>
2024-05-30 08:28:20 +1000

    chore: Update button styles and text in basic chat page

--- app/examples/basic-chat/page.tsx
+++ app/examples/basic-chat/page.tsx
@@ -15,14 +15,14 @@ const Home = () => {
     <main className={styles.main}>
       <div className={styles.container}>
         <div className={styles.welcome}>
-          <p>Hi. I'm the Values Narrator AI.</p>
+          <h3>Hi. I'm the Values Narrator AI.</h3>
           <br />
           <p>I use my expert knowledge of personal values to help you explore personal values and how they influence behavior and interactions. I do this through the power of stories. You can ask me about a specific value, or to describe how a combination of values may interact in different settings.</p> 
           <br />
           <p>What would you like to explore?</p>
           </div>
           <div className={styles.buttonHeading}>
-            <h2>Here are some suggestions:</h2>
+            <h3>Here are some suggestions to get started:</h3>
           </div>
           <div className={styles.buttonContainer}>
           <button className={styles.cardButton} onClick={() => handleButtonClick("Provide a scenario that demonstrates how values can cause conflict in the workplace")}>Conflict in the workplace example</button>
